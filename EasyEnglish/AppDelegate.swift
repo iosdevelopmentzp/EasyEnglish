82deeaca3d4c9d4acffc442bcfab0284390836a5
Dmytro Vorko <dmytrovorko@Dmytros-iMac.local>
2021-04-25 17:37:48 +0300

    Initial commit

---
+++ EasyEnglish/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  EasyEnglish
+//
+//  Created by Dmytro Vorko on 25.04.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    internal var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
