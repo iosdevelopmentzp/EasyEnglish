fd013d7fca92d8894a3b6dbcd6d19c63af2681a8
Vorko Dmitriy <dmitriy.vorko@softermii.com>
2021-05-10 23:44:26 +0300

    EE-101: SwiftGen Fonts

--- Modules/AppResources/AppResources/AppResources/AppColors/AppColors.swift
+++ Modules/AppResources/AppResources/AppResources/AppColors/AppColors.swift
@@ -2,11 +2,11 @@
 // Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
 #if os(macOS)
-  import AppKit
+    import AppKit
 #elseif os(iOS)
-  import UIKit
+    import UIKit
 #elseif os(tvOS) || os(watchOS)
-  import UIKit
+    import UIKit
 #endif
 
 // Deprecated typealiases
@@ -19,52 +19,54 @@ public typealias AssetColorTypeAlias = ColorAsset.Color
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public enum AppColors {
-  public static let appWhite = ColorAsset(name: "AppWhite")
+    public static let appWhite = ColorAsset(name: "AppWhite")
 }
+
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
 
 public final class ColorAsset {
-  public fileprivate(set) var name: String
+    public fileprivate(set) var name: String
 
-  #if os(macOS)
-  public typealias Color = NSColor
-  #elseif os(iOS) || os(tvOS) || os(watchOS)
-  public typealias Color = UIColor
-  #endif
+    #if os(macOS)
+        public typealias Color = NSColor
+    #elseif os(iOS) || os(tvOS) || os(watchOS)
+        public typealias Color = UIColor
+    #endif
 
-  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
-  public private(set) lazy var color: Color = {
-    guard let color = Color(asset: self) else {
-      fatalError("Unable to load color asset named \(name).")
+    @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+    public private(set) lazy var color: Color = {
+        guard let color = Color(asset: self) else {
+            fatalError("Unable to load color asset named \(name).")
+        }
+        return color
+    }()
+
+    fileprivate init(name: String) {
+        self.name = name
     }
-    return color
-  }()
-
-  fileprivate init(name: String) {
-    self.name = name
-  }
 }
 
 public extension ColorAsset.Color {
-  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
-  convenience init?(asset: ColorAsset) {
-    let bundle = BundleToken.bundle
-    #if os(iOS) || os(tvOS)
-    self.init(named: asset.name, in: bundle, compatibleWith: nil)
-    #elseif os(macOS)
-    self.init(named: NSColor.Name(asset.name), bundle: bundle)
-    #elseif os(watchOS)
-    self.init(named: asset.name)
-    #endif
-  }
+    @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+    convenience init?(asset: ColorAsset) {
+        let bundle = BundleToken.bundle
+        #if os(iOS) || os(tvOS)
+            self.init(named: asset.name, in: bundle, compatibleWith: nil)
+        #elseif os(macOS)
+            self.init(named: NSColor.Name(asset.name), bundle: bundle)
+        #elseif os(watchOS)
+            self.init(named: asset.name)
+        #endif
+    }
 }
 
 // swiftlint:disable convenience_type
 private final class BundleToken {
-  static let bundle: Bundle = {
-    Bundle(for: BundleToken.self)
-  }()
+    static let bundle: Bundle = {
+        Bundle(for: BundleToken.self)
+    }()
 }
+
 // swiftlint:enable convenience_type
--- Modules/AppResources/AppResources/AppResources/Localization/Strings.swift
+++ Modules/AppResources/AppResources/AppResources/Localization/Strings.swift
@@ -8,24 +8,26 @@ import Foundation
 
 // swiftlint:disable function_parameter_count identifier_name line_length type_body_length
 public enum Strings {
-  /// Test String
-  public static let testModuleFooSome = Strings.tr("Localizable", "testModule.Foo.Some")
+    /// Test String
+    public static let testModuleFooSome = Strings.tr("Localizable", "testModule.Foo.Some")
 }
+
 // swiftlint:enable function_parameter_count identifier_name line_length type_body_length
 
 // MARK: - Implementation Details
 
 extension Strings {
-  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
-    return String(format: format, locale: Locale.current, arguments: args)
-  }
+    private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+        let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+        return String(format: format, locale: Locale.current, arguments: args)
+    }
 }
 
 // swiftlint:disable convenience_type
 private final class BundleToken {
-  static let bundle: Bundle = {
-    Bundle(for: BundleToken.self)
-  }()
+    static let bundle: Bundle = {
+        Bundle(for: BundleToken.self)
+    }()
 }
+
 // swiftlint:enable convenience_type
