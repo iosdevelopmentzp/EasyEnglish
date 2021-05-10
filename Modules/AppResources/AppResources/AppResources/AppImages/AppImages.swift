7ba47248c4eb33b153d17d853a1e7119a9671b57
Vorko Dmitriy <dmitriy.vorko@softermii.com>
2021-05-10 23:32:18 +0300

    EE-101: SwiftGen colors

--- Modules/AppResources/AppResources/AppResources/AppImages/AppImages.swift
+++ Modules/AppResources/AppResources/AppResources/AppImages/AppImages.swift
@@ -2,11 +2,11 @@
 // Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
 #if os(macOS)
-  import AppKit
+    import AppKit
 #elseif os(iOS)
-  import UIKit
+    import UIKit
 #elseif os(tvOS) || os(watchOS)
-  import UIKit
+    import UIKit
 #endif
 
 // Deprecated typealiases
@@ -19,56 +19,58 @@ public typealias AssetImageTypeAlias = ImageAsset.Image
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public enum AppImages {
-  public static let testGirlImage = ImageAsset(name: "test.girl.image")
+    public static let testGirlImage = ImageAsset(name: "test.girl.image")
 }
+
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
 
 public struct ImageAsset {
-  public fileprivate(set) var name: String
+    public fileprivate(set) var name: String
 
-  #if os(macOS)
-  public typealias Image = NSImage
-  #elseif os(iOS) || os(tvOS) || os(watchOS)
-  public typealias Image = UIImage
-  #endif
-
-  public var image: Image {
-    let bundle = BundleToken.bundle
-    #if os(iOS) || os(tvOS)
-    let image = Image(named: name, in: bundle, compatibleWith: nil)
-    #elseif os(macOS)
-    let image = bundle.image(forResource: NSImage.Name(name))
-    #elseif os(watchOS)
-    let image = Image(named: name)
+    #if os(macOS)
+        public typealias Image = NSImage
+    #elseif os(iOS) || os(tvOS) || os(watchOS)
+        public typealias Image = UIImage
     #endif
-    guard let result = image else {
-      fatalError("Unable to load image asset named \(name).")
+
+    public var image: Image {
+        let bundle = BundleToken.bundle
+        #if os(iOS) || os(tvOS)
+            let image = Image(named: name, in: bundle, compatibleWith: nil)
+        #elseif os(macOS)
+            let image = bundle.image(forResource: NSImage.Name(name))
+        #elseif os(watchOS)
+            let image = Image(named: name)
+        #endif
+        guard let result = image else {
+            fatalError("Unable to load image asset named \(name).")
+        }
+        return result
     }
-    return result
-  }
 }
 
 public extension ImageAsset.Image {
-  @available(macOS, deprecated,
-    message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
-  convenience init?(asset: ImageAsset) {
-    #if os(iOS) || os(tvOS)
-    let bundle = BundleToken.bundle
-    self.init(named: asset.name, in: bundle, compatibleWith: nil)
-    #elseif os(macOS)
-    self.init(named: NSImage.Name(asset.name))
-    #elseif os(watchOS)
-    self.init(named: asset.name)
-    #endif
-  }
+    @available(macOS, deprecated,
+               message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
+    convenience init?(asset: ImageAsset) {
+        #if os(iOS) || os(tvOS)
+            let bundle = BundleToken.bundle
+            self.init(named: asset.name, in: bundle, compatibleWith: nil)
+        #elseif os(macOS)
+            self.init(named: NSImage.Name(asset.name))
+        #elseif os(watchOS)
+            self.init(named: asset.name)
+        #endif
+    }
 }
 
 // swiftlint:disable convenience_type
 private final class BundleToken {
-  static let bundle: Bundle = {
-    Bundle(for: BundleToken.self)
-  }()
+    static let bundle: Bundle = {
+        Bundle(for: BundleToken.self)
+    }()
 }
+
 // swiftlint:enable convenience_type
